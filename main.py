3fc01c4106d48e811ea83c6cc9899b9f96dc2610
Earendil <1466395946@qq.com>
2023-01-03 10:46:55 +0800

    use rich

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from requests import get
 from itertools import accumulate
+from rich.table import Table
+from rich.console import Console
 import time
 import json
 import os
 import sys
 
-type = 'short'
+type = 'long'
 media_id = sys.argv[1]
-base_url = f'https://api.bilibili.com/pgc/review/{type}/list?media_id={media_id}&ps=20&sort=0'
+sort = 0 if len(sys.argv) <= 2 else int(sys.argv[2])
+base_url = f'https://api.bilibili.com/pgc/review/{type}/list?media_id={media_id}&ps=20&sort={sort}'
 result = {
     'short': [0, 0, 0, 0, 0, 0],
     'long': [0, 0, 0, 0, 0, 0]
 }
 next = 0
+cons = Console()
 
 if sys.platform == 'win32':
     clear = lambda: os.system('cls')
@@ -26,26 +30,31 @@ def average_(v):
     return (v[2] * 4 + v[3] * 6 + v[4] * 8 + v[5] * 10) / sum(v[2:])
 
 def run(cursor):
+    time.sleep(0)
     res = json.loads(get(f'{base_url}&cursor={cursor}').text)['data']
+    ctime = 0
     for i in res['list']:
         # if i['score'] == 0:
         #     print(i)
         #     input()
         result[type][i['score'] // 2] += 1
-    clear()
-    time.sleep(0)
+        ctime = i['ctime']
+    cons.clear()
+    scores = Table('类型', *map(lambda x: f'{x}星', range(6)), '平均分', '平均分(去掉1星)', '人数', title='评分')
     for k, v in result.items():
-        print(f'{"长" if k == "long" else "短"}评:')
-        for s, i in enumerate(v):
-            print(f'    打{s}星的人数: {i}')
-        if sum(v) != 0:
-            print('    平均分:', average(v))
-            print('    平均分(不包括1星)', average_(v))
-        print('    人数:', sum(v))
+        scores.add_row(
+            f'{"长" if k == "long" else "短"}评',
+            *map(str, v),
+            *map(lambda x: f'[red]{round(x, 4)}[/red]', (average(v), average_(v)) if sum(v) else (0, 0)),
+            f'[red]{sum(v)}[/red]'
+        )
+    cons.print(scores)
+    if sort:
+        print('当前时间:', time.ctime(ctime))
     total = [a + b for a, b in zip(*result.values())]
-    print('总平均分:', average(total))
-    print('总平均分(不包括1星)', average_(total))
-    print('总人数:', sum(total))
+    cons.print(f'[i]总平均分[i]: [red]{average(total)}[/red]')
+    cons.print(f'[i]总平均分(不包括1星)[i]: [red]{average_(total)}[/red]')
+    cons.print(f'[i]总人数[i]: [red]{sum(total)}[/red]')
     if sum(result[type]) >= res['total']:
         return False
     global next
@@ -56,12 +65,12 @@ def start():
     global next, type, base_url
     while True:
         if not run(next):
-            if type == 'short':
-                type = 'long'
-                base_url = f'https://api.bilibili.com/pgc/review/{type}/list?media_id={media_id}&ps=20&sort=0'
+            if type == 'long':
+                type = 'short'
+                base_url = f'https://api.bilibili.com/pgc/review/{type}/list?media_id={media_id}&ps=20&sort={sort}'
                 next = 0
             else:
-                print('结束')
+                cons.print('[u]结束[/u]')
                 break
 
 if __name__ == '__main__':
